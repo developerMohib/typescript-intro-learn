11cf36fd510e7885fa8590c75bbf1972a204d0b3
developerMohib <mohibullahmohim2020@gmail.com>
2024-11-08 02:03:38 +0600

    promise function added

--- module/dist/test.js
+++ module/dist/test.js
@@ -1,3 +1,74 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 const name2 = "mohibullah mohim 2";
 console.log(name2);
+{
+    const createPromise = () => {
+        return new Promise((resolve, reject) => {
+            const data = { something: "something" };
+            try {
+                if (data) {
+                    resolve(data);
+                }
+                else {
+                    reject("Failed to reload data");
+                }
+            }
+            catch (error) {
+                console.log(error.message);
+            }
+        });
+    };
+    const callPromise = () => __awaiter(void 0, void 0, void 0, function* () {
+        const data = yield createPromise();
+        console.log('data ', data);
+        return data;
+    });
+    // console.log(callPromise())
+    callPromise();
+    const sendEmailPromise = (email) => {
+        return new Promise((resolve, reject) => {
+            const success = true;
+            try {
+                if (success) {
+                    resolve("Your email send successfully");
+                }
+                else {
+                    reject('Failed to send email to you');
+                }
+            }
+            catch (error) {
+                console.log(reject({ message: "Failed to send email" }));
+            }
+        });
+    };
+    const notifyUser = () => __awaiter(void 0, void 0, void 0, function* () {
+        const email = { to: "ex@gmail.com", subject: "A important query to you", body: "Thank you for submitting" };
+        try {
+            const result = yield sendEmailPromise(email);
+            console.log(result);
+            return result;
+        }
+        catch (error) {
+            console.log(error.message);
+        }
+    });
+    notifyUser();
+    // to da fetching data
+    const getToDoPost = () => __awaiter(void 0, void 0, void 0, function* () {
+        const todoPost = yield fetch("https://jsonplaceholder.typicode.com/posts");
+        const post = yield todoPost.json();
+        console.log('post ', post);
+        return post;
+    });
+    getToDoPost();
+    // end here
+}
--- module/src/test.ts
+++ module/src/test.ts
@@ -1,2 +1,98 @@
-const name2 : string = "mohibullah mohim 2"
+const name2: string = "mohibullah mohim 2"
 console.log(name2)
+
+{
+    // start here
+
+
+
+    // create a newe promise
+    type CustomError = {
+        message: string
+    }
+    type Something = {
+        something: string;
+    }
+
+    const createPromise = (): Promise<Something> => {
+        return new Promise<Something>((resolve, reject) => {
+            const data: Something = { something: "something" };
+            try {
+                if (data) {
+                    resolve(data)
+                } else {
+                    reject("Failed to reload data")
+                }
+
+            } catch (error) {
+                console.log((error as CustomError).message)
+            }
+        })
+    }
+
+    const callPromise = async (): Promise<Something> => {
+        const data: Something = await createPromise()
+        console.log('data ', data)
+        return data
+    }
+    // console.log(callPromise())
+    callPromise()
+
+
+    // send email to user and admin
+
+    type Email = {
+        to: string;
+        subject: string;
+        body: string;
+    }
+
+    const sendEmailPromise = (email: Email): Promise<string> => {
+        return new Promise<string>((resolve, reject) => {
+            const success: boolean = true;
+            try {
+                if (success) {
+                    resolve("Your email send successfully")
+                } else {
+                    reject('Failed to send email to you')
+                }
+            } catch (error) {
+                console.log(reject({ message: "Failed to send email" } as CustomError))
+            }
+        })
+    }
+    const notifyUser = async (): Promise<string | void> => {
+        const email: Email = { to: "ex@gmail.com", subject: "A important query to you", body: "Thank you for submitting" }
+        try {
+            const result = await sendEmailPromise(email)
+            console.log(result)
+            return result
+        } catch (error) {
+            console.log((error as CustomError).message)
+        }
+    }
+    notifyUser()
+
+    // to da fetching data
+    interface Post {
+        body: string;
+        id: number;
+        title: string;
+        userId: number;
+    }
+
+    const getToDoPost = async () : Promise<string> => {
+        const todoPost = await fetch("https://jsonplaceholder.typicode.com/posts");
+        const post = await todoPost.json()
+        console.log('post ', post)
+        return post
+    }
+    getToDoPost()
+
+
+
+
+
+
+    // end here
+}
